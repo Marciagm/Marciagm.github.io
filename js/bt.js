1c5a48c184796ed7d6d61a3defb6c4d84bfa0cd1
Marcia <guommiao1989@yeah.net>
2017-12-19 21:37:40 +0800

    add game part

--- js/bt.js
+++ js/bt.js
@@ -7,6 +7,44 @@ var images = [
     'images/christamas/finish.png'
 ];
 var timer;
+function nextPage (pageId) {
+    if (pageId === 'page2') {
+        // page2
+        $('#page2').show();
+        $('#barrier').animate({
+            left: '-95.26rem'
+        }, {
+            duration: 20000,
+            easing: 'linear',
+            step: function (now, tween) {
+                if (now <= -8.26) {
+                    console.log(now);
+                    console.log(tween);
+                }
+            },
+            complete: function () {
+                console.log('barrier end');
+                //$('#game').hide();
+                /*$('.gift').show();
+                $('#santa').hide();
+                $('.gift').click(function () {
+                    console.log('gist');
+                    $('#B').show();
+                    document.getElementById('B').play();
+                    $('.gift').hide();
+                })*/
+                //$('#ad').show();
+            }
+        })
+        $('#page2').click(function () {
+            $('#santa').animate({
+                top: '5.61rem'
+            }, ).animate({
+                top: '8.61rem'
+            })
+        }) 
+    }
+}
 /**
  * loading images and move
  */
@@ -24,7 +62,7 @@ function move (percent) {
             setTimeout(function () {
                 $('#loading').hide();
                 $('#page1').show();
-                beforePlayVideo('video1');
+                beforePlayVideo('video1', 'page2');
             }, 200)
         }    
     }) 
@@ -59,7 +97,7 @@ function loading (images) {
     }, 50)      
 }
 
-function beforePlayVideo (videoId) {
+function beforePlayVideo (videoId, nextPageId) {
     var startY = 0;
     var moveY = 0; 
     var endY = 0;
@@ -92,7 +130,9 @@ function beforePlayVideo (videoId) {
             $('#next').hide();
             videoItem.play();
             videoItem.addEventListener('ended', function () {
-                console.log('end');
+                $('loading').show();
+                $('#' + videoId).hide();
+                nextPage(nextPageId);
             })
         }
     })
